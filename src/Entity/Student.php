1c2b91d31f253c3bca6cdf77dcbf3a1ba40d94df
endloww <aurelien.blanchet74@gmail.com>
2023-10-19 10:26:27 +0200

    Initial commit

---
+++ src/Entity/Student.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+class Student extends User
+{
+    #[ORM\OneToMany(mappedBy: 'student', targetEntity: Grade::class, orphanRemoval: true)]
+    private Collection $grades;
+
+    #[ORM\ManyToOne(inversedBy: 'students')]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?ClassLevel $classLevel = null;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->grades = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Grade>
+     */
+    public function getGrades(): Collection
+    {
+        return $this->grades;
+    }
+
+    public function addGrade(Grade $grade): static
+    {
+        if (!$this->grades->contains($grade)) {
+            $this->grades->add($grade);
+            $grade->setStudent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGrade(Grade $grade): static
+    {
+        if ($this->grades->removeElement($grade)) {
+            // set the owning side to null (unless already changed)
+            if ($grade->getStudent() === $this) {
+                $grade->setStudent(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getClassLevel(): ?ClassLevel
+    {
+        return $this->classLevel;
+    }
+
+    public function setClassLevel(?ClassLevel $classLevel): static
+    {
+        $this->classLevel = $classLevel;
+
+        return $this;
+    }
+
+    public function getGradeByEval (Evaluation $evaluation): ?Grade
+    {
+        foreach ($this->getGrades() as $grade){
+            if ($grade->getEvaluation() === $evaluation){
+                return $grade;
+            }
+        }
+        return null;
+    }
+    public function Moyenne(): ?float
+    {
+        $total = 0;
+        $count = 0;
+
+        foreach ($this->grades as $grade) {
+            $total += $grade->getGrade();
+            $count++;
+        }
+
+        if ($count === 0) {
+            return null;
+        }
+
+        return $total / $count;
+    }
+}
